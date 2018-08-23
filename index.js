10db670463f61c9ee88376cf877b6cf645a0059e
ngyl88 <ng_yl88@hotmail.com>
2018-08-23 18:12:27 +0800

    adding models and users endpoint from ref;

---
+++ index.js
@@ -0,0 +1,25 @@
+// dependencies to get models
+const { User, Blog, Tag } = require('./sequelize')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+// API ENDPOINTS
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`)
+})
+
+// create a user
+app.post('/api/users', (req, res) => {
+    User.create(req.body)
+        .then(user => res.json(user))
+})
+// get all users
+app.get('/api/users', (req, res) => {
+    User.findAll().then(users => res.json(users))
+})
